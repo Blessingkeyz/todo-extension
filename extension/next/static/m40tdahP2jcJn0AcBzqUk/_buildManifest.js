5643b5c83c85d4450d456b13fbf22fd53cf44cf4
Blessingkeyz <umukoroblessing@gmail.com>
2022-12-13 01:10:53 +0100

    extension creator scripts

---
+++ extension/next/static/m40tdahP2jcJn0AcBzqUk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/384-8bf4cdada2ba79c0.js","static/chunks/pages/index-7b66cf8e27e8e4a4.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
